3b929eeae1d4ab5bb569445db3e766a6d10895ee
Catherine Liang <catherineliang39@gmail.com>
2020-04-01 17:53:08 -0400

    Changed url in Person.js to test MongoDB atlas connection

--- WebPart/Person.js
+++ WebPart/Person.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 
 // the host:port must match the location where you are running MongoDB
 // the "myDatabase" part can be anything you like
-mongoose.connect('mongodb://localhost:27017/myDatabase');
+mongoose.connect('mongodb+srv://projectanon:abcd1234@cluster0-rvr54.mongodb.net/test?retryWrites=true&w=majority');
 
 var Schema = mongoose.Schema;
 
