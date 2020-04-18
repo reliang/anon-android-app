c8d1b2ac5f0cdd92f7da5fc8d9ddf41dfe38026c
Catherine Liang <catherineliang39@gmail.com>
2020-04-18 15:20:48 -0400

    Fixed post-click crash

--- AndroidPart/app/src/main/java/upenn/edu/cis350/anon/ProfileActivity.java
+++ AndroidPart/app/src/main/java/upenn/edu/cis350/anon/ProfileActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -17,6 +18,7 @@ import upenn.edu.cis350.anon.ui.chat.PostListAdapter;
 public class ProfileActivity extends AppCompatActivity {
 
     User user;
+    public static PostListAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,11 +43,24 @@ public class ProfileActivity extends AppCompatActivity {
             followers.setText(Integer.toString(user.getNumFollowers()));
             // set post list
             ListView postlist = (ListView) findViewById(R.id.profile_post_list);
-            PostListAdapter adapter = new PostListAdapter(user.getPostsWritten());
+            adapter = new PostListAdapter(user.getPostsWritten());
             postlist.setAdapter(adapter);
-
+            postlist.setOnItemClickListener(listClick);
         }
     }
+
+    private AdapterView.OnItemClickListener listClick = new AdapterView.OnItemClickListener () {
+        @Override
+        public void onItemClick(AdapterView parent, View v, int position, long id) {
+            Intent i = new Intent(parent.getContext(), PostActivity.class);
+            Post postClicked = adapter.getItem( position );
+            if (postClicked != null) {
+                i.putExtra("post", postClicked);
+                startActivity(i);
+            }
+        }
+
+    };
 
     public void onFollowingClick(View v) {
         Intent i = new Intent(this, UserListActivity.class);
